1acf7a103ad8aedc86549c11b1b40cd09857516e
suwidnyana <grayfullbuster522@gmail.com>
2020-09-24 00:03:04 +0800

    change app.js from class to function

--- src/App.js
+++ src/App.js
@@ -1,42 +1,57 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect } from 'react';
 import styles from './App.module.css'
 
 import { Cards, Chart, CountryPicker } from './components';
 import { fethcData } from './api'
-class App extends Component {
 
-    state = {
-        data: {},
-        country : ''
-    }
+const App = () => {
+
+    const [country, setCountry] = useState('')
+    const [data, setData] = useState({})
+
+
+    // state = {
+    //     data: {},
+    //     country : ''
+    // }
     
-    async componentDidMount() {
-        const fetchedData = await fethcData();
+    // async componentDidMount() {
+    //     const fetchedData = await fethcData();
 
-        this.setState({ data: fetchedData });
-    }
+    //     this.setState({ data: fetchedData });
+    // }
 
-    handleCountryChange = async (country) => {
+     useEffect(() => {
+       async function fetchedData() {
+          const fetch = await fethcData();
+            setData(fetch);
+       }
+       fetchedData()
+      },[])
+    
+    const handleCountryChange = async (country) => {
         const fetchedData = await fethcData(country);
        
-        this.setState({
-            data: fetchedData, country: country
-        })
+        // this.setState({
+        //     data: fetchedData, country: country
+        // })
+
+        setData(fetchedData)
+        setCountry(country)
         
     }
 
-    render()
-    {
+   
 
-        const { data,country } = this.state;
+        // const { data,country } = this.state;
         return (
             <div className={styles.container}>
                <Cards data={data}/>
-                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <CountryPicker handleCountryChange={handleCountryChange}/>
                 <Chart data={data} country={country}/>
             </div>
         )
-    }
+   
 }
 
 export default App;
